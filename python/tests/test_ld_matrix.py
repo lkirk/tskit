fix normalisation and a small indexing bug. dimension tests

python/tests/test_ld_matrix.py
@@ -25,7 +25,6 @@ Test cases for two-locus statistics
 
 import contextlib
 import io
-import itertools
 from dataclasses import dataclass
 from itertools import combinations_with_replacement
 from itertools import permutations
@@ -244,6 +243,41 @@ def norm_hap_weighted(
     for k in range(result_dim):
         n = sample_set_sizes[k]
         result[k] = hap_weights[0, k] / n
+
+
+def norm_hap_weighted_ij(
+    result_dim: int,
+    hap_weights: np.ndarray,
+    n_a: int,
+    n_b: int,
+    result: np.ndarray,
+    params: Dict[str, Any],
+) -> None:
+    # """Create a vector of normalizing coefficients, length of the number of
+    # sample sets. In this normalization strategy, we weight each allele's
+    # statistic by the proportion of the haplotype present.
+    """
+    TODO!
+
+    :param result_dim: Number of dimensions in output. Dependent on arity of stat.
+    :param hap_weights: Proportion of each two-locus haplotype.
+    :param n_a: Number of alleles at the A locus.
+    :param n_b: Number of alleles at the B locus.
+    :param result: Result vector to store the normalizing coefficients in.
+    :param params: Params of summary function.
+    """
+    del n_a, n_b  # handle unused params
+    sample_set_sizes = params["sample_set_sizes"]
+    set_indexes = params["set_indexes"]
+    # TODO: k should be ???
+    for k in range(result_dim):
+        i = set_indexes[k][0]
+        j = set_indexes[k][1]
+        ni = sample_set_sizes[i]
+        nj = sample_set_sizes[j]
+        wAB_i = hap_weights[0, i]
+        wAB_j = hap_weights[0, j]
+        result[k] = (wAB_i / ni / 2) + (wAB_j / nj / 2)
 
 
 def norm_total_weighted(
@@ -558,6 +592,9 @@ def compute_general_two_site_stat_result(
             for k in range(result_dim):
                 result[k] += result_tmp[k] * norm[k]
 
+            # for k in range(result_dim):
+            #     print(mut_a, mut_b, k, weights[0, k], weights[1, k], weights[2, k], sep="\t")
+
 
 def two_site_count_stat(
     ts: tskit.TreeSequence,
@@ -810,7 +847,6 @@ def two_locus_count_stat(
     norm_func,
     polarised,
     mode,
-    result_dim,
     sites=None,
     positions=None,
     sample_sets=None,
@@ -826,9 +862,6 @@ def two_locus_count_stat(
     :param polarised: If true, skip the computation of the statistic for the
                       ancestral state.
     :param mode: Whether or not to compute "site" or "branch" statistics.
-    :param result_dim: The dimensions of the output array. For one-way stats,
-                       this will be the number of sample sets. For two-way stats,
-                       the number of index tuples.
     :param sites: List of two lists containing [row_sites, column_sites].
     :param positions: List of two lists containing [row_positions, col_positions],
                       which are genomic positions to compute LD on.
@@ -1007,18 +1040,18 @@ def r2_ij_summary_func(
         i = set_indexes[k][0]
         j = set_indexes[k][1]
         n = sample_set_sizes[i]
-        p_AB = state[0, k] / n
-        p_Ab = state[1, k] / n
-        p_aB = state[2, k] / n
+        p_AB = state[0, i] / n
+        p_Ab = state[1, i] / n
+        p_aB = state[2, i] / n
         p_A = p_AB + p_Ab
         p_B = p_AB + p_aB
         D_i = p_AB - (p_A * p_B)
         denom_i = np.sqrt(p_A * p_B * (1 - p_A) * (1 - p_B))
 
         n = sample_set_sizes[j]
-        p_AB = state[0, k] / n
-        p_Ab = state[1, k] / n
-        p_aB = state[2, k] / n
+        p_AB = state[0, j] / n
+        p_Ab = state[1, j] / n
+        p_aB = state[2, j] / n
         p_A = p_AB + p_Ab
         p_B = p_AB + p_aB
         D_j = p_AB - (p_A * p_B)
@@ -1255,17 +1288,17 @@ def D2_ij_summary_func(
         j = set_indexes[k][1]
 
         n = sample_set_sizes[i]
-        p_AB = state[0, k] / n
-        p_Ab = state[1, k] / n
-        p_aB = state[2, k] / n
+        p_AB = state[0, i] / n
+        p_Ab = state[1, i] / n
+        p_aB = state[2, i] / n
         p_A = p_AB + p_Ab
         p_B = p_AB + p_aB
         D_i = p_AB - (p_A * p_B)
 
         n = sample_set_sizes[j]
-        p_AB = state[0, k] / n
-        p_Ab = state[1, k] / n
-        p_aB = state[2, k] / n
+        p_AB = state[0, j] / n
+        p_Ab = state[1, j] / n
+        p_aB = state[2, j] / n
         p_A = p_AB + p_Ab
         p_B = p_AB + p_aB
         D_j = p_AB - (p_A * p_B)
@@ -1357,7 +1390,7 @@ NORM_METHOD = {
     D2_unbiased_summary_func: norm_total_weighted,
     Dz_unbiased_summary_func: norm_total_weighted,
     pi2_unbiased_summary_func: norm_total_weighted,
-    r2_ij_summary_func: norm_hap_weighted,
+    r2_ij_summary_func: norm_hap_weighted_ij,
     D2_ij_summary_func: norm_total_weighted,
     D2_ij_unbiased_summary_func: norm_total_weighted,
 }
@@ -1379,9 +1412,11 @@ POLARIZATION = {
 }
 
 
-def check_set_indexes(num_sets: int, num_set_indexes: int, set_indexes: np.ndarray):
-    for i in range(len(set_indexes)):
-        for j in range(num_set_indexes):
+def check_set_indexes(
+    num_sets: int, num_set_indexes: int, tuple_size: int, set_indexes: np.ndarray
+):
+    for i in range(num_set_indexes):
+        for j in range(tuple_size):
             if set_indexes[i, j] < 0 or set_indexes[i, j] >= num_sets:
                 raise ValueError(f"Bad sample set index: {set_indexes[i, j]}")
 
@@ -1399,7 +1434,7 @@ def check_sample_stat_inputs(
         )
     if num_index_tuples < 1:
         raise ValueError(f"Insufficient number of index tuples: {num_index_tuples}")
-    check_set_indexes(num_sample_sets, num_index_tuples, index_tuples)
+    check_set_indexes(num_sample_sets, num_index_tuples, tuple_size, index_tuples)
 
 
 def ld_matrix(
@@ -1421,7 +1456,6 @@ def ld_matrix(
         NORM_METHOD[summary_func],
         POLARIZATION[summary_func],
         mode,
-        result_dim,
         sites=sites,
         positions=positions,
         indexes=indexes,
@@ -1744,6 +1778,7 @@ def test_ld_empty_examples(ts):
 
 
 def test_input_validation():
+    # TODO
     ts = get_paper_ex_ts()
     with pytest.raises(ValueError, match="Unknown two-locus statistic"):
         ts.ld_matrix(stat="bad_stat")
@@ -2159,6 +2194,49 @@ def test_branch_ld_matrix_2pop_sample_sets_unbiased(ts, sample_set, stat):
     )
 
 
+def gen_dims_test_cases(ts, mode):
+    ss = ts.samples()
+    dim = ts.num_sites if mode == "site" else ts.num_trees
+    base = (dim, dim)
+    return [
+        {"name": f"{mode}_default", "ld_params": {"mode": mode}, "shape": base},
+        {
+            "name": f"{mode}_dim_drop",
+            "ld_params": {"mode": mode, "sample_sets": ss},
+            "shape": base,
+        },
+        {
+            "name": f"{mode}_no_dim_drop",
+            "ld_params": {"mode": mode, "sample_sets": [ss]},
+            "shape": (1, *base),
+        },
+        {
+            "name": f"{mode}_two_sample_sets",
+            "ld_params": {"mode": mode, "sample_sets": [ss, ss]},
+            "shape": (2, *base),
+        },
+        {
+            "name": f"{mode}_two_way_dim_drop",
+            "ld_params": {"mode": mode, "sample_sets": [ss, ss], "indexes": (0, 1)},
+            "shape": base,
+        },
+        {
+            "name": f"{mode}_two_way_no_dim_drop",
+            "ld_params": {"mode": mode, "sample_sets": [ss, ss], "indexes": [(0, 1)]},
+            "shape": (1, *base),
+        },
+        {
+            "name": f"{mode}_two_way_three_set_indexes",
+            "ld_params": {
+                "mode": mode,
+                "sample_sets": [ss, ss],
+                "indexes": [(0, 0), (0, 1), (1, 1)],
+            },
+            "shape": (3, *base),
+        },
+    ]
+
+
 def get_test_dims_test_cases():
     test_cases = {
         "empty_tree",
@@ -2168,16 +2246,26 @@ def get_test_dims_test_cases():
         "internal_nodes_samples",
         "mixed_internal_leaf_samples",
     }
-    return [t for t in get_example_tree_sequences() if t.id in test_cases]
+    for ts_case in [t for t in get_example_tree_sequences() if t.id in test_cases]:
+        ts = ts_case.values[0]
+        for dim_case in gen_dims_test_cases(ts, "site"):
+            name = "_".join([dim_case["name"], ts_case.id])
+            yield pytest.param(ts, dim_case["ld_params"], dim_case["shape"], id=name)
+        for dim_case in gen_dims_test_cases(ts, "branch"):
+            name = "_".join([dim_case["name"], ts_case.id])
+            yield pytest.param(ts, dim_case["ld_params"], dim_case["shape"], id=name)
 
 
-@pytest.mark.parametrize("ts", get_test_dims_test_cases())
-def test_dims(ts):
-    ss = ts.samples()
-    assert ld_matrix(ts).ndim == 2
-    assert ld_matrix(ts, sample_sets=ss).ndim == 2
-    assert ld_matrix(ts, sample_sets=[ss]).ndim == 3
-    assert ld_matrix(ts, sample_sets=[ss, ss]).ndim == 3
-    assert ld_matrix(ts, sample_sets=[ss, ss], indexes=(0, 0)).ndim == 2
-    assert ld_matrix(ts, sample_sets=[ss, ss], indexes=[(0, 0)]).ndim == 3
-    assert ld_matrix(ts, sample_sets=[ss, ss], indexes=[(0, 0), (0, 1)]).ndim == 3
+@pytest.mark.parametrize("ts,params,shape", get_test_dims_test_cases())
+def test_dims(ts, params, shape):
+    assert ts.ld_matrix(**params).shape == ld_matrix(ts, **params).shape == shape
+
+
+# TODO
+@pytest.mark.parametrize("ts,sample_sets", get_test_branch_2pop_test_cases())
+@pytest.mark.parametrize("stat", sorted(TWO_WAY_SUMMARY_FUNCS.keys()))
+def test_two_way_branch_ld_matrix(ts, sample_sets, stat):
+    np.testing.assert_array_almost_equal(
+        ld_matrix(ts, sample_sets=sample_sets, indexes=[(0, 0), (0, 1), (1, 1)]),
+        ts.ld_matrix(sample_sets=sample_sets, indexes=[(0, 0), (0, 1), (1, 1)]),
+    )