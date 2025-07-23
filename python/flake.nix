850418be532a9eb5cd4ef01997f4621e705ccc94
lkirk <kirklloyd@gmail.com>
2025-07-23 06:06:04 -0500

    forgot these args. needed to use installed packages

--- python/flake.nix
+++ python/flake.nix
@@ -1,19 +1,26 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-
   outputs =
     { self, nixpkgs }@inputs:
     let
       system = "x86_64-linux";
       pkgs = import inputs.nixpkgs { inherit system; };
     in
-    (pkgs.buildFHSEnv {
-      name = "python";
-      targetPkgs = pkgs: [
-        pkgs.python311Full
-        pkgs.gcc
-        pkgs.pkg-config
-      ];
-      runScript = "bash";
-    }).env;
+    {
+      devShells.${system}.default =
+        (pkgs.buildFHSEnv {
+          name = "python";
+          targetPkgs = pkgs: [
+            pkgs.gcc
+	    pkgs.gsl
+	    pkgs.gsl.dev
+            pkgs.libz
+	    pkgs.meson
+	    pkgs.ninja
+            pkgs.pkg-config
+            pkgs.python313Full
+          ];
+          runScript = "fish";
+        }).env;
+    };
 }
